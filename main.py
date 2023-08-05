86a67aeda6336492ed744236181167b680811f3b
harlesky <49336838+harlesky@users.noreply.github.com>
2023-08-05 12:51:03 +0000

    dynamic data and api route
    added dynamic data, use of bootstrap in styling the web and using jsonify

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Scientist',
+    'location': 'lagos, Nigeria',
+    'salary': 'N.130,000'
+  },
+  {
+    'id': 2,
+    'title': 'Frontend Engineer',
+    'location': 'Ogun, Nigeria',
+    'salary': 'N.120,000'
+  },
+  {
+    'id': 3,
+    'title': 'Backend Engineer',
+    'location': 'NY, USA',
+    'salary': '$200,000'
+  }  
+]
+
 @app.route("/")
 def hello():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
